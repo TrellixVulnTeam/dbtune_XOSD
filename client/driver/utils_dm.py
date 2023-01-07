9d2e434ad80f497694668f9dcac057db2cf28495
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 12:16:40 +0000

    Adding tarfile member sanitization to extractall()

--- client/driver/utils_dm.py
+++ client/driver/utils_dm.py
@@ -308,7 +308,26 @@ def copy_file_from_pod(api, namespace="dmcp-instance", name=None, container="dat
                 subdir_and_files = [
                     tarinfo for tarinfo in tar.getmembers()
                 ]
-                tar.extractall(path=dest_path, members=subdir_and_files)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, path=dest_path, members=subdir_and_files)
     except Exception as e:
         print("copy_file_from_pod err: {}".format(e))
         raise Exception("copy_file_from_pod err! {}".format(e))
